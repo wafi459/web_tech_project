22681274786f5cd71701abcaeb8d0e155ace3b12
wafi459 <miuuuu092@gmail.com>
2024-05-21 20:18:54 +0600

    Add files via upload

---
+++ RealEstate/Labib/views/Buyer-dashboard.php
@@ -0,0 +1,97 @@
+<?php
+//session_start();
+//$_SESSION;
+include ("../controllers/check_login.php");
+
+	if (!isset( $_SESSION['username'] )) {
+		header("Location: login.php");
+		exit();
+	}
+?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="design.css" rel="stylesheet">
+    <title>Home Page</title>
+</head>
+<body>
+  <div class="div1">
+ <br><br><br>
+    
+    <button type="submit" class="fixd"> Property</button>
+    <br><br>
+    <a href="../views/Favourite.php">
+    <button type="submit"> Favorites</button></a>
+ <br><br>
+    <button type="submit">Communication</button>
+    <br><br>
+    <button type="submit">Transaction</button>
+    <br><br>
+    <button type="submit">Support</button>
+   
+    
+     </div> 
+     <div class="container">
+
+
+ <div class="all-property">
+  <div class="title">
+  <table>
+      <tr>
+        <td><input type="text">Search</td>
+        <td><a href="updatePass.php" ><button type="submit"><img src="resetpass.png" alt="" width="45" height="45">Update Password</button></a></td>
+         <td><form action="../views/profile.php" method="post">
+    <input type="hidden" name="$_SESSION['username']" >
+   <button type="submit"><img src="profileImg.png" alt="" width="45" height="45"><?php echo  $_SESSION['username'] ; ?></button></form></td>
+   <td><a href="../controllers/logout.php">
+     <button type="submit"><img src="logoutImg.png" alt="" width="45" height="45">Log out</button></a> </td>
+      </tr>
+   
+  </table> 
+
+  </div>
+ 
+
+<?php
+
+
+$servername = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "my_app";
+
+
+$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT property_name , price ,location , details  FROM PROPERTY";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+   
+    while($row = mysqli_fetch_assoc($result)) {
+       echo "<br>"."<fieldset >"."<table>"."<tr>"."<td >"."<b>"."Property Name: "."</b>" . $row["property_name"]."</td>". "<td>"."<b>"."  Price: " ."</b>".$row["price"]."</td>"."<td>";
+       echo '<button class="buy">Buy</button></td>';
+       echo '<td><button class="save"><a href="../controllers/Favourite.php">Save</a></button></td>';
+       echo "</tr>";
+       echo  "<tr>". "<td>"."<b>". "  Location: " ."</b>". $row["location"]. "</td>"."<td>". "<b>"."  Property Details: " ."</b>". $row["details"]. "</td>";
+       echo '<td><button class="contact">Contact</button></td>';
+       echo "</tr>"."</table>"."</fieldset>";
+
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+?>
+
+</div>
+</div> 
+</body>
+</html>
---
+++ RealEstate/Labib/views/forgotPass.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <link href="dashboard.css" rel="stylesheet">
+ <script  src="signupJS.js"></script>
+</head>
+<body>
+<h1 align="center">Forgot Password</h1>
+<form action="../controllers/checkForgotpass.php " method="post" onsubmit="return jsValidate2();" novalidate >
+  
+        <table align="center">
+            <tr>
+            <td>
+                
+                  <fieldset id="fld3">
+                      
+                      <table width="">
+                          <tr>
+                                <td><p align="center">  <label for="uname">Username:</p></td></label>
+                                  <td> <input type="text" id="uname" name="uname"></td>
+                                  <td>  <span id="error1"></span>  </td></tr>
+
+
+                                <tr>
+                                <td><p align="center"> <br>  <label for="emial">Email:</label></p></td>
+                                  <td><br><input type="text" id="email" name="email"></td>
+                                  <td>  <span id="error2"></span>  </td></tr>
+                                
+                                <tr>
+                                <td><p > <br> <label for="pass">Password:</p></td></label>
+                                  <td><br><input type="password" id="pass" name="pass"></td>
+                                  <td>  <span id="error3"></span>  </td></tr> 
+                                  
+                                  <tr>   
+                            <td>    </td>  <td align="center"><input type="submit" value="Reset" id="bt3"></td></tr>
+                            <tr><td colspan="1"><a href="login.php" >Go to Login</a></td></tr>
+
+                                    
+                              </table>  
+                  </fieldset>
+                  <?php
+		echo isset($_SESSION['message1']) ? $_SESSION['message1'] : "";
+	?>
+                                  </td></tr></table></form>
+                                                
+         
+                        
+                        
+
+</body>
+</html>
